56de21cd3f3438683c6d5269a3d81b7ce2477a83
LOGANATHANAN <anloganathan20@gmail.com>
2021-11-07 07:25:59 +0530

    updated comment box

--- public/forumApp.js
+++ public/forumApp.js
@@ -2,14 +2,16 @@ function loadComments(id) {
     const xhttp = new XMLHttpRequest();
     console.log(id);
     xhttp.onload = function() {
-    var ele=document.getElementById(id);
-    
+    var ele=document.getElementById('Comments'+id);
+    ele.parentNode.style.display="block";
+    ele.textContent='';
+    ele.style.display="block";
     var array=JSON.parse(this.responseText);
     for(var i=0;i<array.length;i++){
         var row=document.createElement('div');
-        row.setAttribute('class','row');
+        row.setAttribute('class','row w3-panel w3-border w3-round-xlarge');
         var col=document.createElement('div');
-        col.setAttribute('class','col-12');
+        col.setAttribute('class','col-10');
         var user=document.createElement('div');
         var uText=document.createTextNode(array[i].commentBy +" on "+array[i].time);
         user.appendChild(uText);
